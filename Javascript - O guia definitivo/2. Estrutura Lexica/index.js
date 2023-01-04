f790c7f370b4349bee7593db73a67ecb2b162f86
Leonardo dos Santos <leodoima@hotmail.com>
2023-01-04 18:28:29 -0300

    Exemplos tratados no capítulo 2

---
+++ Javascript - O guia definitivo/2. Estrutura Lexica/index.js
@@ -0,0 +1,32 @@
+// Concatenar Strings e Numbers
+var numberA = 10;
+var numberB = '20';
+var consolidNumbers = numberA + numberB;
+console.log(consolidNumbers);
+
+
+// Trata-se de uma linguagem case-sensitive
+var user = 'Leonardo';
+var login = 'leonardo';
+
+var compareNameUser = (user == login) ? true : false;
+console.log(compareNameUser);
+
+
+// Não se faz necessário o uso de ponto e vírgula (;), porém, é recomendado para melhor organização do código
+var houseNumber
+houseNumber
+=
+1056
+console.log(houseNumber)
+
+
+// Exemplo de comportamento indevido por conta da falta de ponto e vírgula e identação (;). X ou Y devem incrementar?
+var x = 0
+var y = 0
+x
+++
+y
+console.log(x);
+console.log(y);
+
